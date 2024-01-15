fdf380f79412ba62635be4443660c7dafb068165
sabzj <zjsabbah@gmail.com>
2024-01-15 11:15:17 +0200

    added openai

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-
+import getOpenAiInstance from "./openAi.js";
 const server = express();
 
 import { errorHandler } from "./middleware/errorHandler.js";
@@ -16,6 +16,37 @@ server.use(express.json());
 server.use("/api/v1/activities", activityRoutes);
 server.use("/api/v1/users", userRoutes);
 
+app.post("/api/message", async (req, res) => {
+  const openai = getOpenAiInstance();
+  try {
+    const userMessage = req.body.message;
+
+    res.setHeader("Content-Type", "text/plain");
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      max_tokens: 500,
+      messages: [
+        {
+          role: "system",
+          content: `reply with info how the activity ${req.body.activityName} is useful to the society and the community`,
+        },
+        { role: "user", content: userMessage },
+      ],
+      stream: true,
+      flush_interval: 1000,
+    });
+
+    for (const chunk of response.stream) {
+      res.write(chunk.choices[0].message.content);
+    }
+
+    res.end();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 server.use(errorHandler);
 
 const PORT = process.env.PORT || 6000;
